d494f6d1e390e0081e5824d786101775feac2726
shubhamguptasg <41472791+shubhamguptasg@users.noreply.github.com>
2021-01-10 12:00:06 -0800

    Update customer.service.ts

--- Angular3/src/app/customer.service.ts
+++ Angular3/src/app/customer.service.ts
@@ -8,18 +8,25 @@ import { Customer } from './customer';
   providedIn: 'root'
 })
 export class CustomerService {
-private url="http://localhost:53226/api/";
-//http://localhost:53226/api/AddNewUser
+private url='http://localhost:53226/api/';
+//http://localhost:53226/api/Auth/AddNewUser
   constructor(private http:HttpClient) { }
     
   getusers():Observable<Customer[]>{
 return this.http.get<Customer[]>(this.url+"GetUsers");
    }
  
-   adduser(user:Customer){  
-    //const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };  
-    this.http.post<Customer>(this.url+"AddNewUser,",user);
-    //, httpOptions);  
-  }  
+   adduser(user:Customer):Observable<Customer>{  
+    return this.http.post<Customer>(this.url+'AddNewUser',user,{
+      headers:new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    });
+    //.pipe(catchError(this.handleError));  
+    }  
+  
+  
+
+  
 
 }
