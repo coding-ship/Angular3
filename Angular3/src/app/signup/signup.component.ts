a1e029093c6fe293f4eb0688eef8af9f86fedfe4
shubhamguptasg <41472791+shubhamguptasg@users.noreply.github.com>
2021-01-10 11:57:50 -0800

    Update signup.component.ts

--- Angular3/src/app/signup/signup.component.ts
+++ Angular3/src/app/signup/signup.component.ts
@@ -16,14 +16,16 @@ export class SignupComponent {
   ngOnInit(): void {
   }
   onsubmit(customer:Customer){
+    
     window.alert("Account Successfully Created Now You Can Login!");
     console.log(customer);
     console.log(customer.fname);
     console.log(customer.lname);
     console.log(customer.phone);
     console.log(customer.role);
-    this.Customerservice.adduser(customer);
-    
+    //this.Customerservice.adduser(customer);
+    this.Customerservice.adduser(customer).subscribe(response=>{this.value=response},(error:any)=>console.log(error));
+   
     
 
   }
